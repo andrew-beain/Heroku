c2078cebf46955257e541cb4b9d1f9e7eb8b34a1
andrew-beain <andrew.beain@mymail.champlain.edu>
2013-11-13 17:54:55 -0500

    server modifications

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var x =
+{
+    "name": "Andrew Beain",
+    "vacation": {
+        "places": ["38.6833, 109.5667", "55.7500, 37.6167"]
+    }
+};
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.get('/', function (req, res) {
+    res.send({ name: x });
 });
 
 var port = process.env.PORT || 5000;
